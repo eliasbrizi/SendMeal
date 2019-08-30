3f2588fade79f6a4fac59b16dd9c7971d5a8c852
Elias Brizi <eliasbrizi@yahoo.es>
2019-08-30 08:34:46 -0300

    visibilidad layout vendedor

--- app/src/main/java/com/B3B/sendmeal/MainActivity.java
+++ app/src/main/java/com/B3B/sendmeal/MainActivity.java
@@ -3,6 +3,9 @@ package com.B3B.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Switch sw = (Switch)findViewById(R.id.switchVendedor);
+        final LinearLayout layoutVendedor = (LinearLayout)findViewById(R.id.layoutVendedor);
+
+        sw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sw.isChecked()){
+                    layoutVendedor.setVisibility(View.VISIBLE);
+                } else layoutVendedor.setVisibility(View.GONE);
+
+            }
+        });
     }
 }

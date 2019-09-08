cfd4b57e3446f4fd1dbea25f8bb22a4f0387c4ce
Elias Brizi <eliasbrizi@yahoo.es>
2019-09-08 11:49:17 -0300

    slider + valor inicial credito

--- app/src/main/java/com/B3B/sendmeal/MainActivity.java
+++ app/src/main/java/com/B3B/sendmeal/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static java.lang.Integer.valueOf;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    /*
+    *   Variables de pantalla
+    */
         final Switch sw = (Switch)findViewById(R.id.switchVendedor);
         final LinearLayout layoutVendedor = (LinearLayout)findViewById(R.id.layoutVendedor);
-
+        final SeekBar seekCredito = (SeekBar)findViewById(R.id.seekCredito);
+        final TextView textValorCredito = (TextView) findViewById(R.id.textValorCredito);
+    /*
+    *
+    */
+        seekCredito.setMax(1400);
         sw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +36,24 @@ public class MainActivity extends AppCompatActivity {
                     layoutVendedor.setVisibility(View.VISIBLE);
                 } else layoutVendedor.setVisibility(View.GONE);
 
+            }
+        });
+        seekCredito.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                // TODO Auto-generated method stub
+                textValorCredito.setText(String.valueOf(100 + new Integer(progress)));
             }
         });
     }

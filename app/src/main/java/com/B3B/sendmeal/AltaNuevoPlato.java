69e89ebff44a62031bd800e8e2346d1650631956
Elias Brizi <eliasbrizi@yahoo.es>
2019-09-24 19:51:48 -0300

    Agregar validaciones a nuevo plato

--- app/src/main/java/com/B3B/sendmeal/AltaNuevoPlato.java
+++ app/src/main/java/com/B3B/sendmeal/AltaNuevoPlato.java
@@ -1,6 +1,11 @@
 package com.B3B.sendmeal;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,5 +18,33 @@ public class AltaNuevoPlato extends AppCompatActivity {
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarBack);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*
+        Variables de pantalla
+         */
+        final EditText editIdPlato = (EditText) findViewById(R.id.editNumIDANP);
+        final EditText editTituloPlato = (EditText) findViewById(R.id.editTituloANP);
+        final EditText editDescripcionPlato = (EditText) findViewById(R.id.editDescripcionANP);
+        final EditText editPrecio = (EditText) findViewById(R.id.editPrecioANP);
+        final EditText editCalorias = (EditText) findViewById(R.id.editCaloriasANP);
+
+        final Button btnGuardarPlato = (Button) findViewById(R.id.buttonGuardarPlato);
+
+        /*
+
+         */
+
+        btnGuardarPlato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editCalorias.getText().toString().equals("") ||
+                editDescripcionPlato.getText().toString().equals("") ||
+                editIdPlato.getText().toString().equals("") ||
+                editPrecio.getText().toString().equals("") ||
+                editTituloPlato.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(),R.string.faltanDatos,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

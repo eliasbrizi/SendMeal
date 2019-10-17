9e249820ba1da6925bc5ce1752143e33b9f6105c
FrancoBrechbuhl <37877874+FrancoBrechbuhl@users.noreply.github.com>
2019-10-17 19:36:48 -0300

    notificaciones avanzadas

--- app/src/main/java/com/B3B/sendmeal/OfertaBroadcastReceiver.java
+++ app/src/main/java/com/B3B/sendmeal/OfertaBroadcastReceiver.java
@@ -2,12 +2,14 @@ package com.B3B.sendmeal;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class OfertaBroadcastReceiver extends BroadcastReceiver {
     public static final String OFERTA = "On Sale";
@@ -18,11 +20,19 @@ public class OfertaBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         contexto = context;
         //Toast.makeText(context,"El plato " + intent.getExtras().getString("nombrePlato") + " se encuentra en OFERTA!!",Toast.LENGTH_SHORT).show();
+        Intent intento = new Intent(contexto, ListaPlatos.class);
+        intento.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(contexto, 0, intento, 0);
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(contexto, "CH1")
                 .setSmallIcon(R.drawable.icono_notify).setContentTitle(contexto.getString(R.string.nombreNotificacionOferta))
                 .setContentText(intent.getExtras().getString("nombrePlato") + contexto.getString(R.string.detalleNotificacionOferta))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
 
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(contexto);
+        notificationManagerCompat.notify(99, mBuilder.build());
     }
 
     private void createNotificationChannel(){

d9942cc5814d80c7901e7c7a8e634c4714f7ce67
FrancoBrechbuhl <37877874+FrancoBrechbuhl@users.noreply.github.com>
2019-10-17 23:18:04 -0300

    notificaciones finalizadas

--- app/src/main/java/com/B3B/sendmeal/OfertaBroadcastReceiver.java
+++ app/src/main/java/com/B3B/sendmeal/OfertaBroadcastReceiver.java
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -15,34 +16,36 @@ public class OfertaBroadcastReceiver extends BroadcastReceiver {
     public static final String OFERTA = "On Sale";
 
     private Context contexto;
+    final String CHANNEL1 = "CH1";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         contexto = context;
-        //Toast.makeText(context,"El plato " + intent.getExtras().getString("nombrePlato") + " se encuentra en OFERTA!!",Toast.LENGTH_SHORT).show();
-        Intent intento = new Intent(contexto, ListaPlatos.class);
+        Intent intento = new Intent(context, ListaPlatos.class);
         intento.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(contexto, 0, intento, 0);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intento, 0);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(contexto, "CH1")
-                .setSmallIcon(R.drawable.icono_notify).setContentTitle(contexto.getString(R.string.nombreNotificacionOferta))
-                .setContentText(intent.getExtras().getString("nombrePlato") + contexto.getString(R.string.detalleNotificacionOferta))
+        createNotificationChannel();
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL1)
+                .setSmallIcon(R.drawable.icono_notify).setContentTitle(context.getString(R.string.nombreNotificacionOferta))
+                .setContentText(intent.getExtras().getString("nombrePlato") + context.getString(R.string.detalleNotificacionOferta))
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
 
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(contexto);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
         notificationManagerCompat.notify(99, mBuilder.build());
     }
 
     private void createNotificationChannel(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            final String CANAL_ID = "CH1";
             CharSequence name = contexto.getString(R.string.nombreCanal);
             String descripcion = contexto.getString(R.string.descripcionCanal);
             int importancia = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel canal = new NotificationChannel(CANAL_ID, name, importancia);
+            NotificationChannel canal = new NotificationChannel(CHANNEL1, name, importancia);
             canal.setImportance(importancia);
+
             NotificationManager notificationManager = contexto.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(canal);
         }

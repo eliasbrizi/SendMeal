4b68f19be4cba5133993e9e07ed572da88d6569d
FrancoBrechbuhl <37877874+FrancoBrechbuhl@users.noreply.github.com>
2019-10-22 12:10:55 -0300

    metodos en PlatoRest

--- app/src/main/java/com/B3B/sendmeal/dao/rest/PlatoRest.java
+++ app/src/main/java/com/B3B/sendmeal/dao/rest/PlatoRest.java
@@ -6,11 +6,22 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface PlatoRest {
     @GET("platos/") Call<List<Plato>> listarTodos();
 
+    @GET("platos/") Call<Plato> findPlatoByID(int id);
+
     @POST("platos/") Call<Plato> crearPlato(@Body Plato p);
+
+    @PATCH("platos/") Call<Plato> actualizarNombrePlato(String nombre);
+
+    @PATCH("platos/") Call<Plato> actualizarDescripcionPlato(String descripcion);
+
+    @DELETE("platos/") Call<Plato> eliminarPlato(int id);
 }

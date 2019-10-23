6bc9c045bb439b449f3952a68d93a290f16c4287
FrancoBrechbuhl <37877874+FrancoBrechbuhl@users.noreply.github.com>
2019-10-23 10:15:59 -0300

    mapeo de clases Pedido e ItemsPedido a ROOM

--- app/src/main/java/com/B3B/sendmeal/domain/ItemsPedido.java
+++ app/src/main/java/com/B3B/sendmeal/domain/ItemsPedido.java
@@ -1,4 +1,69 @@
 package com.B3B.sendmeal.domain;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(foreignKeys = @ForeignKey(entity = Pedido.class, parentColumns = "id", childColumns = "pedido"))
 public class ItemsPedido {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    private int id;
+    @ColumnInfo(name = "pedido")
+    private Pedido pedido;
+    @Ignore
+    private Plato plato;
+    @ColumnInfo(name = "cantidad")
+    private int cantidad;
+    @ColumnInfo(name = "precio")
+    private double precio;
+
+    public void ItemsPedido(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Plato getPlato() {
+        return plato;
+    }
+
+    public void setPlato(Plato plato) {
+        this.plato = plato;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
--- app/src/main/java/com/B3B/sendmeal/domain/Pedido.java
+++ app/src/main/java/com/B3B/sendmeal/domain/Pedido.java
@@ -1,18 +1,79 @@
 package com.B3B.sendmeal.domain;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Entity(tableName = "PEDIDO")
 public class Pedido {
 
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
     private int id;
+    @ColumnInfo(name = "fecha")
     private LocalDateTime fecha;
+    @ColumnInfo(name = "estado")
     private int estado;
+    @ColumnInfo(name = "latitud")
     private double lat;
+    @ColumnInfo(name = "longitud")
     private double lng;
     private List<ItemsPedido> items;
 
     public void Pedido(){
 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public List<ItemsPedido> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemsPedido> items) {
+        this.items = items;
     }
 }

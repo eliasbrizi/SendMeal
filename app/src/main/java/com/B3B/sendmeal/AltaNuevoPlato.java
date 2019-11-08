489b1fa56513f4f591a21ec2316b9b3dbc59a3a4
FrancoBrechbuhl <37877874+FrancoBrechbuhl@users.noreply.github.com>
2019-11-07 21:41:10 -0300

    cambios en AltaNuevoPlato

--- app/src/main/java/com/B3B/sendmeal/AltaNuevoPlato.java
+++ app/src/main/java/com/B3B/sendmeal/AltaNuevoPlato.java
@@ -1,6 +1,10 @@
 package com.B3B.sendmeal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,11 +12,22 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
 import com.B3B.sendmeal.dao.PlatoRepository;
 import com.B3B.sendmeal.domain.Plato;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AltaNuevoPlato extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_SAVE = 2;
+    static String pathFoto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +44,32 @@ public class AltaNuevoPlato extends AppCompatActivity {
         final EditText editPrecio = (EditText) findViewById(R.id.editPrecioANP);
         final EditText editCalorias = (EditText) findViewById(R.id.editCaloriasANP);
 
+        final Button btnTomarFomar = (Button) findViewById(R.id.btnAgregarFoto);
         final Button btnGuardarPlato = (Button) findViewById(R.id.buttonGuardarPlato);
 
         /*
 
         */
+
+        btnTomarFomar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent takeFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(takeFoto.resolveActivity(getPackageManager()) != null){
+                    File photoFile = null;
+                    try{
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+
+                    }
+                    if(photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(getApplicationContext(), "com.example.android.fileprovider", photoFile);
+                        takeFoto.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takeFoto, REQUEST_IMAGE_SAVE);
+                    }
+                }
+            }
+        });
 
         btnGuardarPlato.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,5 +92,24 @@ public class AltaNuevoPlato extends AppCompatActivity {
                 }
             }
         });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(REQUEST_IMAGE_CAPTURE == requestCode && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+
+        }
+    }
+
+    private File createImageFile () throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", dir);
+        pathFoto = image.getAbsolutePath();
+        return image;
+    }
+
 }
